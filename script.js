52ead6cfd0a71ccc6ed23781ae01b2c4d5530446
Ayesha_Liaquat_Ali <akhanam0318@gmail.com>
2026-07-30 20:55:23 +0500

    Restore unlock button functionality and event listeners

--- script.js
+++ script.js
@@ -113,16 +113,16 @@ document.addEventListener("click", function(e){
 });
 const photos = [
 
-"images/1.jpg",
-"images/2.jpg",
-"images/3.jpg",
-"images/4.jpg",
-"images/5.jpg",
-"images/6.jpg",
-"images/7.jpg",
-"images/8.jpg",
-"images/9.jpg",
-"images/10.jpg"
+"1.jpg",
+"2.jpg",
+"3.jpg",
+"4.jpg",
+"5.jpg",
+"6.jpg",
+"7.jpg",
+"8.jpg",
+"9.jpg",
+"10.jpg"
 
 ];
 
